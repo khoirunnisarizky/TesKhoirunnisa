361b1ba095cec28521015280a70fb7993892862f
Khoirunnisa' Rizky <khoirunnisa.rizky.n@mail.ugm.ac.id>
2019-11-02 20:27:30 +0700

    Initial Commit

---
+++ TesKhoirunnisa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesKhoirunnisa
+//
+//  Created by khoirunnisa' rizky noor fatimah on 02/11/19.
+//  Copyright © 2019 khoirunnisa' rizky noor fatimah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
